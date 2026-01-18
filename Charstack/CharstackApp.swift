bf3174cdc2d5bd8850b2d0019e05e21465043327
bahadirgezer <bahadirgezerbg@gmail.com>
2026-01-18 15:02:44 +0100

    Initial Commit

---
+++ Charstack/CharstackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharstackApp.swift
+//  Charstack
+//
+//  Created by Bahadır Gezer on 18/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct CharstackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
